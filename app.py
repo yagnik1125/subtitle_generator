faaeafd0e7e226382a33a3170e5300c5fbaffb93
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-09-21 14:35:32 +0530

    roll back to transcription

--- app.py
+++ app.py
@@ -299,16 +299,19 @@ if st.button("Transcribe and Translate Audio"):
             )
         # Append the chunk transcription to full transcription
         transcription_segment=transcription.segments
-        translation_segment=list(transcription.segments)
-        for seg in translation_segment:
-            # st.write(seg['text'])
-            seg['text']=translate_text(seg['text'], selected_lang_tar)
 
-        for i in range(len(transcription_segment)):
-            st.write(transcription_segment[i]['text'])
-            st.write(translation_segment[i]['text'])
+        # translation_segment=list(transcription.segments)
+        # for seg in translation_segment:
+        #     # st.write(seg['text'])
+        #     seg['text']=translate_text(seg['text'], selected_lang_tar)
+
+        # for i in range(len(transcription_segment)):
+        #     st.write(transcription_segment[i]['text'])
+        #     st.write(translation_segment[i]['text'])
+
         # st.write(translation_segment)
         # st.write(transcription_segment)
+
         # transcription_text = transcription.text
         # full_transcription += transcription_text + " "
         # --------------------without chunk ends--------------------------------------------------
@@ -355,10 +358,10 @@ if st.button("Transcribe and Translate Audio"):
 
         # #------------------------------------vedio generator--------------------------------------
 
-        # write_vtt(translation_segment, os.path.join("/", vedio_file_name + ".vtt"))
-        # os.system(f'ffmpeg -i "{vedio_file_name}" -vf subtitles="{vedio_file_name}.vtt" "{vedio_file_name}_subtitled.mp4" ')
+        write_vtt(transcription_segment, os.path.join("/", vedio_file_name + ".vtt"))
+        os.system(f'ffmpeg -i "{vedio_file_name}" -vf subtitles="{vedio_file_name}.vtt" "{vedio_file_name}_subtitled.mp4" ')
 
-        # st.video(f"{vedio_file_name}_subtitled.mp4")
+        st.video(f"{vedio_file_name}_subtitled.mp4")
 
     else:
         st.error("Please upload an audio file.")
