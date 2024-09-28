a2cbd525c1ffb741681eaf25b179e6af22d644c8
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-09-28 10:16:59 +0530

    testing

--- pages/vedio2subtitle.py
+++ pages/vedio2subtitle.py
@@ -26,7 +26,9 @@ import av
 import numpy as np
 from twilio.rest import Client
 from pytube import YouTube
+import yt_dlp
 import re
+import traceback
 
 # https://pypi.org/project/streamlit-webrtc/
 # https://github.com/whitphx/streamlit-stt-app/tree/main
@@ -86,38 +88,57 @@ if youtube_link:
         # raise ValueError("Invalid YouTube URL provided.")
     else:
         st.write("Link OK")
+
+    
     # try:
-    #     file_path = yt_dlp_download(yt_url)
+    #     file_path = yt_dlp_download(youtube_link)
     # except Exception as e:
-    #     logging.error(f"generate_youtube_transcript_with_groq failed to download YouTube video from URL {yt_url}: {e}")
-    #     logging.error(traceback.format_exc())
-    #     raise
+    #     st.error(f"generate_youtube_transcript_with_groq failed to download YouTube video from URL {youtube_link}: {e}")
+    #     st.error(traceback.format_exc())
 
-    st.write("Youtube Vedio links: ",youtube_link)
-
-    # try:
-    #     yt = YouTube(youtube_link)
-    #     video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    #     video.download('temp_video.mp4')
-    #     youtube_vedio_file_name = 'temp_video.mp4'
-    #     st.write(f"Video downloaded and saved as: {youtube_vedio_file_name}")
-    # except Exception as e:
-    #     st.error(f"Failed to download YouTube video. Error: {str(e)}")
+    # st.write("Youtube Vedio links: ",youtube_link)
 
 
-# # Function to save video from frames
-# def save_video_from_frames(frames, output_file):
-#     if frames:
-#         height, width, _ = frames[0].shape
-#         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-#         out = cv2.VideoWriter(output_file, fourcc, 30.0, (width, height))
-#         for frame in frames:
-#             out.write(frame)
-#         out.release()
-# webrtc_streamer(key="video_capture", video_transformer_factory=VideoTransformer, 
-#                     video_frame_callback=VideoTransformer.transform)
+# def yt_dlp_download(yt_url:str, output_path:str = None) -> str:
+#     """
+#     Downloads the audio track from a specified YouTube video URL using the yt-dlp library, then converts it to an MP3 format file.
+#     This function configures yt-dlp to extract the best quality audio available and uses FFmpeg (via yt-dlp's postprocessors) to convert the audio to MP3 format. The resulting MP3 file is saved to the specified or default output directory with a filename derived from the video title.
+#     Args:
+#         yt_url (str): The URL of the YouTube video from which audio will be downloaded. This should be a valid YouTube video URL.
+#     Returns:
+#         str: The absolute file path of the downloaded and converted MP3 file. This path includes the filename which is derived from the original video title.
+#     Raises:
+#         yt_dlp.utils.DownloadError: If there is an issue with downloading the video's audio due to reasons such as video unavailability or restrictions.
+        
+#         Exception: For handling unexpected errors during the download and conversion process.
+#     """
+#     if output_path is None:
+#         output_path = os.getcwd()
 
+#     ydl_opts = {
+#         'format': 'bestaudio/best',
+#         'postprocessors': [{
+#             'key': 'FFmpegExtractAudio',
+#             'preferredcodec': 'mp3',
+#             'preferredquality': '192',
+#         }],
+#         'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
+#     }
 
+#     try:
+#         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#             result = ydl.extract_info(yt_url, download=True)
+#             file_name = ydl.prepare_filename(result)
+#             mp3_file_path = file_name.rsplit('.', 1)[0] + '.mp3'
+#             logging.info(f"yt_dlp_download saved YouTube video to file path: {mp3_file_path}")
+#             return mp3_file_path
+#     except yt_dlp.utils.DownloadError as e:
+#         logging.error(f"yt_dlp_download failed to download audio from URL {yt_url}: {e}")
+#         raise
+#     except Exception as e:
+#         logging.error(f"An unexpected error occurred with yt_dlp_download: {e}")
+#         logging.error(traceback.format_exc())
+#         raise
 
 def get_audio_buffer(audio_file):
     with open(audio_file, "rb") as f:
