450a2418e0e08f33771b5ebd68ec27439b8fa32a
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-09-28 10:38:49 +0530

    youtube_video_file_name

--- pages/vedio2subtitle.py
+++ pages/vedio2subtitle.py
@@ -354,7 +354,7 @@ if uploaded_vedio_file is not None:
 youtube_link = st.text_input("Enter YouTube Video Link")
 
 # Download and process YouTube video if the link is provided
-youtube_vedio_file_name=""
+youtube_video_file_name=""
 if youtube_link:
     youtube_url_pattern = r'^(https?://)?(www\.)?(youtube\.com|youtu\.be)/.+$'
     if not re.match(youtube_url_pattern, youtube_link):
@@ -365,12 +365,12 @@ if youtube_link:
 
     
     try:
-        youtube_vedio_file_name = yt_dlp_download(youtube_link)
+        youtube_video_file_name = yt_dlp_download(youtube_link)
     except Exception as e:
         st.error(f"generate_youtube_transcript_with_groq failed to download YouTube video from URL {youtube_link}: {e}")
         st.error(traceback.format_exc())
 
-    st.vedio(youtube_vedio_file_name)
+    st.video(youtube_video_file_name)
     # chunk_size=5*60000
     # temp_dir = "temp_chunks"
     # chunk_files=[]
@@ -410,7 +410,7 @@ if youtube_link:
     #         st.error(error_message)
     #         st.error(traceback.format_exc())
     #         # raise error_message
-    st.write("Youtube Vedio links: ",youtube_link)
+    st.write("Youtube video links: ",youtube_link)
 
 
 selected_lang_tar = st.selectbox("Select the Target language for Subtitle", ['afrikaans', 'albanian', 'amharic', 'arabic', 'armenian', 'azerbaijani', 'basque', 'belarusian', 'bengali', 'bosnian', 'bulgarian', 'catalan', 'cebuano', 'chichewa', 'chinese (simplified)', 'chinese (traditional)', 'corsican', 'croatian', 'czech', 'danish', 'dutch', 'english', 'esperanto', 'estonian', 'filipino', 'finnish', 'french', 'frisian', 'galician', 'georgian', 'german', 'greek', 'gujarati', 'haitian creole', 'hausa', 'hawaiian', 'hebrew', 'hebrew', 'hindi', 'hmong', 'hungarian', 'icelandic', 'igbo', 'indonesian', 'irish', 'italian', 'japanese', 'javanese', 'kannada', 'kazakh', 'khmer', 'korean', 'kurdish (kurmanji)', 'kyrgyz', 'lao', 'latin', 'latvian', 'lithuanian', 'luxembourgish', 'macedonian', 'malagasy', 'malay', 'malayalam', 'maltese', 'maori', 'marathi', 'mongolian', 'myanmar (burmese)', 'nepali', 'norwegian', 'odia', 'pashto', 'persian', 'polish', 'portuguese', 'punjabi', 'romanian', 'russian', 'samoan', 'scots gaelic', 'serbian', 'sesotho', 'shona', 'sindhi', 'sinhala', 'slovak', 'slovenian', 'somali', 'spanish', 'sundanese', 'swahili', 'swedish', 'tajik', 'tamil', 'telugu', 'thai', 'turkish', 'ukrainian', 'urdu', 'uyghur', 'uzbek', 'vietnamese', 'welsh', 'xhosa', 'yiddish', 'yoruba', 'zulu'])
